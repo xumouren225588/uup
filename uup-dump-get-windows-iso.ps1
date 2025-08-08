931eae7391622d8f9ede5d71a364ccc89c3183a1
marcinmajsc <27273303+marcinmajsc@users.noreply.github.com>
2025-08-08 22:41:37 +0200

    Update uup-dump-get-windows-iso.ps1

--- uup-dump-get-windows-iso.ps1
+++ uup-dump-get-windows-iso.ps1
@@ -294,7 +294,6 @@ function Get-WindowsIso($name, $destinationDirectory) {
         -replace '^(AutoExit\s*)=.*','$1=1' `
         -replace '^(ResetBase\s*)=.*','$1=1' `
         -replace '^(Cleanup\s*)=.*','$1=1'
-    $tags = $null
     if ($esd) {
         $convertConfig = $convertConfig -replace '^(wim2esd\s*)=.*', '$1=1'
         $tags = $tags + '.ESD'
